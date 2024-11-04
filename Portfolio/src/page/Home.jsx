cb19367538c5e3330610b1d34018abb536aa7309
Yojan pawade <74175179+yojan24@users.noreply.github.com>
2024-11-04 22:41:09 +0530

    Update Home.jsx

--- Portfolio/src/page/Home.jsx
+++ Portfolio/src/page/Home.jsx
@@ -5,18 +5,11 @@ import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 
 function Home() {
-  useGSAP(() => {
-    gsap.from(".home-animate", {
-      opacity: 0,
-      duration: 1,
-      delay: 1,
-    });
-  });
   return (
     <Element name="home">
       <div className="min-h-[100vh] lg:h-screen py-2 ">
         <Nav />
-        <Intro className="home-animate" />
+        <Intro  />
       </div>
     </Element>
   );
